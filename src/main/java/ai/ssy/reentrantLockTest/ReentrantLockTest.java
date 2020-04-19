79e78121d3fa854691ac7bc8cd3535933267dc54
897645431@qq.com <897645431@qq.com>
2020-04-19 22:37:31 +0800

    ReentrantLock公平锁/非公平锁

---
+++ src/main/java/ai/ssy/reentrantLockTest/ReentrantLockTest.java
@@ -0,0 +1,47 @@
+package ai.ssy.reentrantLockTest;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author:ssy
+ * @Date:
+ * @Description: ReentrantLock公平锁/非公平锁
+ */
+public class ReentrantLockTest {
+
+//    private static Lock lock = new ReentrantLock(true);
+    private static Lock lock = new ReentrantLock(false);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new ThreadDemo(i)).start();
+        }
+    }
+
+    static class ThreadDemo implements Runnable {
+        Integer id;
+
+        public ThreadDemo(Integer id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            try {
+                TimeUnit.MILLISECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 2; i++) {
+                lock.lock();
+                System.out.println("获得锁的线程：" + id);
+                lock.unlock();
+            }
+        }
+    }
+}
+
+
+
