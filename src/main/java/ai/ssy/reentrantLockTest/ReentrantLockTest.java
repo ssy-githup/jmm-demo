edb66adb6dd24c31bb62ef54a39dca0ef3cf1860
897645431@qq.com <897645431@qq.com>
2020-04-19 22:52:37 +0800

    ReentrantLock公平锁/非公平锁

--- src/main/java/ai/ssy/reentrantLockTest/ReentrantLockTest.java
+++ src/main/java/ai/ssy/reentrantLockTest/ReentrantLockTest.java
@@ -10,9 +10,12 @@ import java.util.concurrent.locks.ReentrantLock;
  * @Description: ReentrantLock公平锁/非公平锁
  */
 public class ReentrantLockTest {
-
+    //默认是公平锁：强调公平性；线程执行按照先来后到的
+ //   private static Lock lock = new ReentrantLock();
 //    private static Lock lock = new ReentrantLock(true);
-    private static Lock lock = new ReentrantLock(false);
+    //false：非公平锁；执行lock()时直接抢占锁；提高了系统的吞吐量和并发性
+    //可能会造成：线程优先级颠倒和线程饥饿
+  private static Lock lock = new ReentrantLock(false);
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
@@ -44,4 +47,29 @@ public class ReentrantLockTest {
 }
 
 
+/**
+ * 公平锁的结果：
+ * 获得锁的线程：3
+ * 获得锁的线程：2
+ * 获得锁的线程：0
+ * 获得锁的线程：1
+ * 获得锁的线程：4
+ * 获得锁的线程：3
+ * 获得锁的线程：2
+ * 获得锁的线程：0
+ * 获得锁的线程：1
+ * 获得锁的线程：4
+ *
+ * 非公平锁的结果：
+ * 获得锁的线程：3
+ * 获得锁的线程：3
+ * 获得锁的线程：1
+ * 获得锁的线程：1
+ * 获得锁的线程：0
+ * 获得锁的线程：0
+ * 获得锁的线程：2
+ * 获得锁的线程：2
+ * 获得锁的线程：4
+ * 获得锁的线程：4
+ */
 
